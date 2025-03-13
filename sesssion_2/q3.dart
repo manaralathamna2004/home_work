6d7ba35c00cf5bdbb122e60f11d9b4f888262609
manaralathamna2004 <manaralathamna295@gmail.com>
2025-03-13 14:29:19 +0200

    done

--- sesssion_2/q3.dart
+++ sesssion_2/q3.dart
@@ -1,6 +1,6 @@
 void main(){
   Set<int>uniqueNumbers={10,20,30,40};
-  print("Length of uniqueNumbers:{uniqueNumbers.Length}");
+  print("Length of uniqueNumbers:${uniqueNumbers.length}");
   bool ispersent =uniqueNumbers.contains(20);
   print("Is 20 in uniqueNumber?is present");
 
